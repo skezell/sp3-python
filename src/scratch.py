baa20c0d6fc4b87a59d28d28a79e6c7fd7b2238a
Suzanne Ezell <suzanne.kimber.ezell@gmail.com>
2024-12-23 21:43:55 -0700

    took out commented out lines, bit of cleanup

--- src/scratch.py
+++ src/scratch.py
@@ -2,7 +2,7 @@
 from hub import light_matrix, button, motion_sensor, port
 import runloop
 import time
-import motor, motor_pair
+import motor, motor_pair, color_sensor
 import sys
 
 
@@ -36,67 +36,74 @@ class Logger:
     logging_level = LOG_LEVEL_DEBUG
     logging_level_name = LOG_LEVEL_MAP.get(logging_level, "0")
 
-    hack_diff_val = None
+    hack_values = {}
 
     @classmethod
-    def log(cls, msg, level=LOG_LEVEL_INFO, **kwargs):
+    def log(cls, msg, *args, level=LOG_LEVEL_INFO, **kwargs):
         if level >= cls.logging_level:
             epoch = str(time.ticks_ms())
-            msg = msg.format(**kwargs)
+            msg = msg.format(*args, **kwargs)
             logging_level_name = cls.LOG_LEVEL_MAP.get(level, "0")
             formatted = "[{ts:7}]: [{level:6}]: {msg}".format(
                 ts=epoch, level=logging_level_name, msg=msg
             )
+            # maybe someday we can write to a log file, like a big boy program
             print(formatted)
 
     @classmethod
     def set_level(cls, new_level):
+        # TODO: add check to make sure new level is valid
         cls.logging_level = new_level
         cls.logging_level_name = cls.LOG_LEVEL_MAP.get(
             cls.logging_level, "LEVEL NOT FOUND"
         )
 
     @classmethod
-    def debug(cls, msg, **kwargs):
-        cls.log(msg, level=cls.LOG_LEVEL_DEBUG, **kwargs)
+    def debug(cls, msg, *args, **kwargs):
+        cls.log(msg, *args, level=cls.LOG_LEVEL_DEBUG, **kwargs)
 
     @classmethod
-    def info(cls, msg, **kwargs):
-        cls.log(msg, level=cls.LOG_LEVEL_INFO, **kwargs)
+    def info(cls, msg, *args, **kwargs):
+        cls.log(msg, *args, level=cls.LOG_LEVEL_INFO, **kwargs)
 
     @classmethod
-    def warn(cls, msg, **kwargs):
-        cls.log(msg, level=cls.LOG_LEVEL_WARNING, **kwargs)
+    def warn(cls, msg, *args, **kwargs):
+        cls.log(msg, *args, level=cls.LOG_LEVEL_WARNING, **kwargs)
 
     @classmethod
-    def error(cls, msg, **kwargs):
-        cls.log(msg, level=cls.LOG_LEVEL_ERROR, **kwargs)
+    def error(cls, msg, *args, **kwargs):
+        cls.log(msg, *args, level=cls.LOG_LEVEL_ERROR, **kwargs)
 
     @classmethod
-    def critical(cls, msg, **kwargs):
-        cls.log(msg, level=cls.LOG_LEVEL_CRITICAL, **kwargs)
+    def critical(cls, msg, *args, **kwargs):
+        cls.log(msg, *args, level=cls.LOG_LEVEL_CRITICAL, **kwargs)
 
+    # TODO: really this should be a separate metrics store class & methods
     @classmethod
-    def print_new_value(cls, msg, new_value, level=LOG_LEVEL_DEBUG, **kwargs):
+    def print_new_value(cls, label, new_value, *args, level=LOG_LEVEL_DEBUG, **kwargs):
         # only print if value has changed, cuts down on output from tight loops
-        if new_value != cls.hack_diff_val:
-            new_msg = msg + str(new_value)
-            cls.log(new_msg, level=level, **kwargs)
+        hack_diff_value = cls.hack_values.get(label)
+        if new_value != hack_diff_value:
+            new_msg = label + " == " + str(new_value)
+            cls.log(new_msg, *args, level=level, **kwargs)
             cls.hack_diff_val = new_value
 
 
 class PID:
 
-    def __init__(self, tuning, setpoint: float, initial_input=0, limits=(14, 100)):
+    def __init__(
+        self, name, initial_input=0, setpoint=0, tuning=(0, 0, 0), limits=(0, 0)
+    ):
+        self.name = name
         self.tuning = tuning
         self.limits = limits
 
-        # variables for holding the state
+        # variables for holding the target state
         self.setpoint = setpoint
         self.last_error = self.setpoint - initial_input
 
         # set initial state for PID
-        self.reset()
+        self.reset(initial_input=initial_input)
 
     def reset(self, initial_input=0):
         self.error_sum = 0
@@ -140,14 +147,17 @@ def lookup_max_drive_velocity(motor_size):
     return CONSTANTS.MOTOR_SPEED_MAPPINGS.get(motor_size, 0)
 
 
-def calculate_velocity(percentage=0.75, motor_size=CONSTANTS.MOTOR_SIZE_MED):
-    return int(percentage * lookup_max_drive_velocity(motor_size))
+def calculate_velocity(power=75, motor_size=CONSTANTS.MOTOR_SIZE_MED):
+    return int(power / 100 * lookup_max_drive_velocity(motor_size))
 
 
 def cm_to_degress(cm, wheel_size):
     circumference = CONSTANTS.WHEEL_CIRCUM_MAPPINGS.get(wheel_size, 0)
-    print("cm to degrees {}".format(360 / circumference * cm))
     return 360 / circumference * cm
+
+
+def inches_to_degrees(inches, wheel_size):
+    return cm_to_degress(inches * 2.54, wheel_size)
 
 
 # -------------------------------------------- ROBOT CLASSES --------------------------------------------
@@ -181,7 +191,9 @@ class Robot:
 
         # cache these to avoid calculating lots of times
         self.max_velocity = lookup_max_drive_velocity(self.drive_motor_size)
-        self.default_velocity = calculate_velocity(0.75, self.drive_motor_size)
+        self.default_velocity = calculate_velocity(
+            power=75, motor_size=self.drive_motor_size
+        )
 
         self.pair_drive_motors()
 
@@ -212,9 +224,10 @@ class Robot:
             msg = "Invalid target angle {}".format(target_angle)
             raise ValueError(msg)
 
-    async def tank_turn(
-        self, target_angle, direction="right", starting_power=40, pid_controller=None
-    ):
+    # TODO: add pivot turn & add turn method that also takes the turn type: tank or pivot
+    # TODO: support greater than 180 degrees & let you specify them in 0-360 + direction
+    # TODO: should we support providing a terminator also -- e.g. turn until 90 degrees or hit black???
+    async def tank_turn(self, target_angle, direction="right", starting_power=40):
         # only 0 - 180 is currently supported
         # direction can only be "left" or "right"
         # power 0 - 100, percentage of max to use -- this translates to initial speed
@@ -223,9 +236,7 @@ class Robot:
 
         await self.reset_yaw()
         target_angle = target_angle * 10  # tilt_angles are measured in decigrees
-        pid_controller = pid_controller or self.pid_turning(
-            target_angle, starting_power
-        )
+        pid_controller = self.pid_turning(target_angle, upper_limit=starting_power)
         Logger.debug(
             "Starting yaw == {}, target == {}".format(self.yawstr(), target_angle)
         )
@@ -244,9 +255,12 @@ class Robot:
 
             # we need to convert the power value to a velocity, which is what the move_tank
             # method needs
-            velocity = calculate_velocity(percentage=controller_output / 100)
+            velocity = calculate_velocity(
+                power=int(controller_output), motor_size=self.drive_motor_size
+            )
             motor_pair.move_tank(self.drive_motor_pair, velocity * lvm, velocity * rvm)
-            if yaw >= target_angle:
+            Logger.print_new_value("actual yaw", yaw)
+            if abs(yaw) >= abs(target_angle):
                 return True
             else:
                 return False
@@ -261,31 +275,56 @@ class Robot:
         # convenience method to create a pid for the tank turn maneuver
         # robots that inherit from this class may want to override this method
         # to provide different tuning data
-        tuning = (0.5, 0, 0)
+        tuning = (0.4, 0, 0)
         limits = (14, upper_limit)
-        pid = PID(tuning, setpoint, initial_input=0, limits=limits)
+        pid = PID(
+            "turning_velocity",
+            initial_input=0,
+            setpoint=setpoint,
+            tuning=tuning,
+            limits=limits,
+        )
 
         return pid
 
-    def pid_drive_straight(self, setpoint, lower_limit=-100, upper_limit=100):
+    # TODO: tune 2nd & 3rd parameters
+    def pid_drive_straight(self, setpoint=0, lower_limit=-100, upper_limit=100):
         # convenience method to create a pid for the drive straight method
         # robots that inherit from this class may want to override this method
         # to provide different tuning data
         tuning = (0.1, 0, 0)
-        limits = (-100, 100)
-        pid = PID(tuning, setpoint, initial_input=0, limits=limits)
+        limits = (lower_limit, upper_limit)
+        pid = PID(
+            "steering_straight",
+            initial_input=0,
+            setpoint=setpoint,
+            tuning=tuning,
+            limits=limits,
+        )
 
         return pid
 
-    def create_pid(self, label, initial_input, setpoint, tuning, limits):
-        pid = PID(tuning, setpoint, initial_input, limits)
-        self.pids[label] = pid
+    def create_pid(self, name, initial_input, setpoint, tuning, limits):
+        pid = PID(
+            name,
+            initial_input=initial_input,
+            setpoint=setpoint,
+            tuning=tuning,
+            limits=limits,
+        )
+        self.pids[name] = pid  # register it TODO: not sure this is needed ??
         return pid
 
     async def drive_straight_distance(
-        self, distance_cm, direction="forward", power=40, acceleration=500
+        self, cm=None, inches=None, direction="forward", power=40, acceleration=500
     ):
-        target = cm_to_degress(distance_cm, self.wheel_size)
+        if cm:
+            target = cm_to_degress(cm, self.wheel_size)
+        elif inches:
+            target = inches_to_degrees(inches, self.wheel_size)
+        else:
+            raise ValueError("Either cm or inches are required.")
+
         motor.reset_relative_position(self.left_drive_motor, 0)
 
         def terminator():
@@ -297,7 +336,6 @@ class Robot:
                 return False
 
         await robot.drive_straight(
-            distance_cm,
             direction=direction,
             terminator=terminator,
             power=power,
@@ -306,20 +344,16 @@ class Robot:
 
     async def drive_straight(
         self,
-        distance_cm,
         direction="forward",
-        power=40,
         terminator=None,
-        acceleration=1000,
+        power=40,
+        acceleration=500,
     ):
         Logger.debug("Starting yaw == {}".format(self.yawstr()))
-        pid = self.create_pid("drive_straight", 0, 0, (0.1, 0, 0), (-100, 100))
-        velocity = calculate_velocity(power / 100, self.drive_motor_size)
+        pid = self.pid_drive_straight()
+        velocity = calculate_velocity(power=power, motor_size=self.drive_motor_size)
         await self.reset_yaw()
-        # motor.reset_relative_position(self.left_drive_motor, 0)
 
-        # elsewhere, sensors are reporting decidegrees
-        target_degrees = cm_to_degress(distance_cm, self.wheel_size)
         # TODO: change this to a validation function
         velocity = abs(velocity) if direction == "forward" else abs(velocity) * -1
 
@@ -335,11 +369,7 @@ class Robot:
                 velocity=velocity,
                 acceleration=acceleration,
             )
-            # distance_traveled = abs(motor.relative_position(self.left_drive_motor))
-            # if distance_traveled >= target_degrees:# measured in decidegrees
-            #     return True
-            # else:
-            #     return False
+
             return terminator()
 
         # wait until target angle is reached, then stop the motor pair
@@ -353,7 +383,7 @@ class Robot:
         await runloop.until(motion_sensor.stable)
         motion_sensor.reset_yaw(0)
         # wait a small amount for the code to finish running before using gyro
-        time.sleep_ms(50)
+        time.sleep_ms(20)
 
     @staticmethod
     def yaw():
@@ -365,12 +395,13 @@ class Robot:
 
 
 class MyRobot(Robot):
+    # TODO: add in the newer values rather than relying on the defaults from the parent class
     VALUES = {
         "name": "mini",
         "left_drive_motor": port.A,
         "right_drive_motor": port.B,
         "attachment_motor_ports": {"front": port.C},
-        "sensor_ports": {"light": port.E},
+        "sensor_ports": {"color": port.E},
     }
 
     def __init__(self):
@@ -387,16 +418,12 @@ async def main():
 
 
 async def step1():
-    print("Step 1: Static ")
+    print("Step 1: Static")
     print(robot.__dict__)
-    kp = 0.5
-    ki = 0
-    kd = 0
-    pid = PID((kp, ki, kd), 90)
-
-    for angle in range(0, 95):
-        output = pid.compute(angle)
-        print("input: {}, output: {}".format(angle, output))
+    port = robot.sensor_ports.get("color")
+    print(color_sensor.color(port))
+    print(color_sensor.rgbi(port))
+    print(color_sensor.reflection(port))
 
 
 async def step2():
@@ -414,7 +441,15 @@ async def step3():
 
 
 async def step4():
-    pass
+    print("Step 4: Combo Straight & Turns")
+    distance = 20
+    await robot.drive_straight_distance(distance)
+    time.sleep_ms(100)
+    await robot.tank_turn(90)
+    time.sleep_ms(100)
+    await robot.tank_turn(90, direction="left", starting_power=80)
+    time.sleep_ms(100)
+    await robot.drive_straight_distance(distance, direction="backward")
 
 
 async def end_program():
@@ -426,7 +461,7 @@ async def end_program():
 
 # ---------------------------------------------- SEQUENCES ----------------------------------------------
 everything = [main(), step1(), end_program()]
-steps = [step3()]
+steps = [step1()]
 test = [step2()]
 
 # ------------------------------------------------- RUN! -------------------------------------------------
